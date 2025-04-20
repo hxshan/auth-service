115059fc9c01bb6a3f3fa1dc54ee6b3ad351fbbd
hxshan-g <heshan.g@syncturma.com>
2025-04-20 22:00:13 +0530

    Setup

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+// auth-service/src/models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userId:{
+    type:String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "restaurant", "driver", "admin"],
+    required: true,
+  },
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import { corsOptions } from "./config/corsOptions.js";
+import { routes } from "../../user-service/src/routes/index.js";
+
+dotenv.config();
+app.use(express.json());
+app.use(cors(corsOptions));
+
+routes(app)
+
+const PORT = process.env.PORT || 5002;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`User Service running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
