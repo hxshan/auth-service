ca4f5b8818331f4ef1c93411992520a800c49ee3
ishini shavindhya <ishinishavindhya@gmail.com>
2025-04-28 02:32:49 +0530

    Temporary commit of my current work

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,23 +1,24 @@
 const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:5000", 
-    "http://localhost:8000",
-    "http://localhost:5173" 
-  ];
-  
-  export const corsOptions = {
-    origin: function (origin, callback) {
-      console.log("came to this origin")
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Type", "Authorization"],
-    optionsSuccessStatus: 200
-  };
-  
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "http://localhost:8000",
+  "http://localhost:5173"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("came to this origin")
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200
+};
+
+module.exports = { corsOptions };
